c058dbb41df68b61b91f0fc35c6a95c84ffbc4dc
rclaver <rclaver@xtec.cat>
2025-09-29 14:42:03 +0200

    Verificació de la correcció dels noms de fitxer JSON

--- projects/verificaNomsJson.sh
+++ projects/verificaNomsJson.sh
@@ -18,24 +18,24 @@ else
    dirBase="/home/rafael/projectes/wiki18/data/media"
 fi
 
-function recorregut() {
-   dir_actual=$1
-   cd $dir_actual
-   echo -e "dir_actual: ${dir_actual}"
+function cercar_json() {
+   local directori="$1"
+   echo "ruta: $directori"
 
-   for arxiu in $(ls -1 --group-directories-first); do
-      ruta="${dir_actual}/${arxiu}"
-      if [ -d $arxiu ]; then
-         echo -e "\n${CB_BLU}RUTA: ${CB_YLW}${ruta}${C_NONE}"
-         recorregut $arxiu
-         read i
-         cd ..
+   find "$directori" -type f -name "*.json" | while IFS=   read -r arxiu; do
+      local dir_arxiu=$(dirname "$arxiu")
+      local dir_mig=${dir_arxiu##"${dirBase}/"}
+      dir_mig=${dir_mig/\//_}
+      local nom_arxiu=$(basename "$arxiu")
+
+      #echo -e "\n${CB_CYN}Ruta:${C_NONE} $dir_arxiu"
+      #echo -e "${CB_CYN}Mig:${C_NONE} $dir_mig"
+      #echo -e "${CB_WHT}Arxiu JSON:${C_NONE} $nom_arxiu"
+
+      if [[ "${dir_mig}.json" == "$nom_arxiu" ]]; then
+         echo "arxiu $nom_arxiu CORRECTE"
       else
-         echo -e "${CB_CYN}arxiu: ${CB_WHT}$dir_actual/ $arxiu ${C_NONE}"
-         ext=${arxiu##*.}
-         if [[ "$ext" == "json" ]]; then
-            echo -e "arxiu: ${arxiu}"
-         fi
+         echo "Nom INCORRECTE a l'arxiu $dir_arxiu/ $nom_arxiu"
       fi
    done
 }
@@ -43,4 +43,8 @@ function recorregut() {
 ###
 # inici
 #
-recorregut $dirBase
+echo -e "+-------------------------------------------------------------------------------+"
+echo -e "| Verificació de la correcció dels noms de fitxer JSON al directori data/media/ |"
+echo -e "+-------------------------------------------------------------------------------+"
+
+cercar_json "$dirBase"
